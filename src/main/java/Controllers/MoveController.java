3ddfdf89bceb0d8bc8f674764635a5d797bcb408
nickrudenko89 <mikola_rud@mail.ru>
2017-12-19 23:50:09 +0300

    Condition of victory fix

--- src/main/java/Controllers/MoveController.java
+++ src/main/java/Controllers/MoveController.java
@@ -19,12 +19,13 @@ public class MoveController {
     @RequestMapping("/start")
     public String startGame(HttpServletRequest request, Model model) {
         size = Integer.valueOf(request.getParameter("size"));
-        possibleMoves = size*size;
+        possibleMoves = size * size;
         crosses = new ArrayList<Integer>();
         for (int i = 0; i < size * size; i++)
             crosses.add(-1);
         model.addAttribute("size", size);
         model.addAttribute("crosses", crosses);
+        model.addAttribute("haveWinner", false);
         return "/index.jsp";
     }
 
@@ -35,7 +36,8 @@ public class MoveController {
         int randomNumber = -1;
         crosses.set(elementId, 1);
         possibleMoves--;
-        if(possibleMoves!=0) {
+        model = checkWinner(model, "You");
+        if (possibleMoves != 0) {
             for (; ; ) {
                 randomNumber = random.nextInt(size * size);
                 if (crosses.get(randomNumber) == -1)
@@ -43,9 +45,61 @@ public class MoveController {
             }
             crosses.set(randomNumber, 0);
             possibleMoves--;
+            model = checkWinner(model, "Computer");
         }
         model.addAttribute("size", size);
         model.addAttribute("crosses", crosses);
         return "/index.jsp";
+    }
+
+    private Model checkWinner(Model model, String player) {
+        if (checkField()) {
+            model.addAttribute("haveWinner", true);
+            model.addAttribute("winner", player);
+            possibleMoves = 0;
+        } else {
+            model.addAttribute("haveWinner", false);
+        }
+        return model;
+    }
+
+    private boolean checkField() {
+        int[][] field = new int[size][size];
+        int elementCounter = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                field[i][j] = crosses.get(elementCounter);
+                elementCounter++;
+            }
+        }
+        //проверка победы по строкам и по диагоналям 1ч
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; (j + 2) < size; j++) {
+                if (field[i][j] >= 0) {
+                    //по строке
+                    if ((field[i][j] == field[i][j + 1]) && (field[i][j] == field[i][j + 2]))
+                        return true;
+                    //по 1-ой части диагоналей
+                    if ((i + 2) < size)
+                        if ((field[i][j] == field[i + 1][j + 1]) && (field[i][j] == field[i + 2][j + 2]))
+                            return true;
+                }
+            }
+        }
+        //проверка победы по столбцам и по диагоналям 2ч
+        for (int i = (size - 1); (i - 2) >= 0; i--) {
+            for (int j = 0; j < size; j++) {
+                if (field[i][j] >= 0) {
+                    //по столбцу
+                    if ((field[i][j] == field[i - 1][j]) && (field[i][j] == field[i - 2][j]))
+                        return true;
+                    //по 2-ой части диагоналей
+                    if ((j + 2) < size)
+                        if ((field[i][j] == field[i - 1][j + 1]) && (field[i][j] == field[i - 2][j + 2]))
+                            return true;
+                }
+            }
+        }
+        return false;
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,46 +1,66 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-    <head>
-        <style>
-            table {
-                border: 1px solid grey;
-                border-collapse: collapse;
-            }
-            th, td {border: 1px solid grey;}
-            #fieldSize {width: 30px;}
-            a {color: white;}
-        </style>
-        <title>$Title$</title>
-    </head>
-    <body>
-    <table>
-        <c:if test="${crosses==null}">
-            <form action="/do/start" method="post">
-                Размер поля: <input type="text" name="size" id="fieldSize"><br>
-                <input type="submit" value="Начать">
-            </form>
-        </c:if>
-        <c:if test="${crosses!=null}">
-            <c:set var="counter" value="0"/>
-            <tr>
-            <c:forEach items="${crosses}" var="item">
-                <c:if test="${item==-1}">
-                    <td><a href="/do/move?id=${counter}">E</a></td>
+<head>
+    <style>
+        table {
+            border: 1px solid grey;
+            border-collapse: collapse;
+        }
+
+        td {
+            border: 1px solid grey;
+            width: 20px;
+            height: 20px;
+            text-align: center;
+        }
+
+        #fieldSize {
+            width: 30px;
+        }
+
+        #invisible {
+            color: white;
+        }
+    </style>
+    <title>Крестики-нолики</title>
+</head>
+<body>
+<table>
+    <c:if test="${crosses==null}">
+        <b>Крестики-нолики</b><br><br>
+        <form action="/do/start" method="post">
+            Размер поля: <input type="text" name="size" id="fieldSize" pattern="[0-9]"><br>
+            <input type="submit" value="Начать">
+        </form>
+    </c:if>
+    <c:if test="${crosses!=null}">
+        <c:set var="counter" value="0"/>
+        <tr>
+        <c:forEach items="${crosses}" var="item">
+            <c:if test="${item==-1}">
+                <c:if test="${haveWinner==false}">
+                    <td><a id="invisible" href="/do/move?id=${counter}">V</a></td>
                 </c:if>
-                <c:if test="${item==0}">
-                    <td>O</td>
+                <c:if test="${haveWinner==true}">
+                    <td></td>
                 </c:if>
-                <c:if test="${item==1}">
-                    <td>X</td>
-                </c:if>
-                <c:if test="${(counter+1)%size==0}">
-                    </tr>
-                </c:if>
-                <c:set var="counter" value="${counter+1}"/>
-            </c:forEach>
-        </c:if>
-    </table>
-    ${crosses}
-    </body>
+            </c:if>
+            <c:if test="${item==0}">
+                <td>O</td>
+            </c:if>
+            <c:if test="${item==1}">
+                <td>X</td>
+            </c:if>
+            <c:if test="${(counter+1)%size==0}">
+                </tr>
+            </c:if>
+            <c:set var="counter" value="${counter+1}"/>
+        </c:forEach>
+    </c:if>
+</table>
+<c:if test="${haveWinner==true}">
+    ${winner} win!!!<br><a href="/index.jsp">Начать новую игру</a>
+</c:if>
+</body>
 </html>
